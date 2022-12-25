72eeeaccd75ff17af5b203309e6dfd8568e34d8a
vmaevski <vmaevski@mail.ru>
2022-12-25 13:02:10 +0700

    Add HW_S05

--- HW/HW_S05/HW_S05.sql
+++ HW/HW_S05/HW_S05.sql
@@ -1,3 +1,5 @@
+USE VK;
+
 /* - Создайте представление с произвольным SELECT-запросом из прошлых уроков [CREATE VIEW]
 - Выведите данные, используя написанное представление [SELECT]
 - Удалите представление [DROP VIEW]
@@ -17,3 +19,20 @@ CREATE OR REPLACE VIEW number_of_user_groups AS
 SELECT * FROM number_of_user_groups;
 
 DROP VIEW number_of_user_groups;
+
+/* 
+* Сколько новостей (записей в таблице media) у каждого пользователя?
+ Вывести поля: news_count (количество новостей), user_id (номер пользователя), user_email (email пользователя). 
+Попробовать решить с помощью CTE или с помощью обычного JOIN. */
+
+WITH cte_media AS (
+	select 
+		count(*) AS news_count,
+		user_id
+	from media
+	group by user_id
+)
+SELECT news_count, user_id, u.email AS user_email
+FROM cte_media 
+JOIN users as u on u.id = cte_media.user_id
+
